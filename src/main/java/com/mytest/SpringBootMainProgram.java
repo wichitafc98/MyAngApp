26e8e7f4bcc1219f9ddcf0b85e3bdee4b0532662
michael.howard@bnsf.com <michael.howard@bnsf.com>
2019-03-08 22:46:36 -0600

    update

--- src/main/java/com/mytest/SpringBootMainProgram.java
+++ src/main/java/com/mytest/SpringBootMainProgram.java
@@ -1,14 +1,12 @@
 package com.mytest;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories(basePackages ={ "com.mytest.repo"})
-@EntityScan(basePackages ={ "com.mytest.model"})
+@EntityScan(basePackages ={ "com.mytest.controller"})
 public class SpringBootMainProgram {
     public static void main(String[] args) throws Exception {
         new SpringApplication(SpringBootMainProgram .class).run(args);
